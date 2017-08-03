b0cf715c674077a5bfe22e854fae9a00bb3452c3
Ryan Taylor <ryant@eng.utah.edu>
2017-08-03 11:44:51 -0600

    add batching

--- main.py
+++ main.py
@@ -1,37 +1,62 @@
 import csv, sys, datetime
+import pprint
+from itertools import groupby
 from shutil import copyfile, move
 import os
-from template import fill_template, dc_template
+from template import ie_template, dc_template
 
-#check for arguments
-if len(sys.argv) < 3:
-    print("Please pass the input csv as the first argument and the output dir as the second argument")
-    print("Example:\n\tpython main.py \"example/1/inputs/xml/transfer.csv\" \"example/1/outputs\"")
-    sys.exit(1)
+def main():
+    input_csv, input_dir, output_dir = check_arguments()
 
-input_csv  = sys.argv[1]
-input_dir  = os.path.dirname(input_csv)
-output_dir = sys.argv[2]
-output_dir = output_dir + "/" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    with open(input_csv, 'rb') as csvfile:
+        rows = csv.DictReader(csvfile)
 
-with open(input_csv, 'rb') as csvfile:
-    rows = csv.DictReader(csvfile)
-    for row in rows:
-        # first file
-        directory = output_dir
-        
-        if not os.path.exists(directory):
-            os.makedirs(directory + '/content/streams/')
+        batches = get_batches(rows)
 
-        copyfile( input_dir + '/../tifs/' + row['filename'], directory + '/content/streams/' + row['filename'])
-        new_file = directory + '/content/ie.xml'
-        ie_template_string = fill_template(row)
+        for i in batches:
+            batch = batches[i]
 
-        dc_file = directory + '/dc.xml'
-        dc_template_string = dc_template(row)
+            directory = output_dir + "/batch" + str(i)
+            if not os.path.exists(directory):
+                os.makedirs(directory + '/content/streams/')
 
-        with open(new_file, 'w') as output_file:
-            output_file.write(ie_template_string)
+            new_file = directory + '/content/ie.xml'
+            ie_template_string = ie_template(batch)
 
-        with open(dc_file, 'w') as output_file:
-            output_file.write(dc_template_string)
+            dc_file = directory + '/dc.xml'
+            dc_template_string = dc_template(batch)
+
+            with open(new_file, 'w') as output_file:
+                output_file.write(ie_template_string)
+
+            with open(dc_file, 'w') as output_file:
+                output_file.write(dc_template_string)
+
+            for row in batch:
+                copyfile( input_dir + '/../tifs/' + row['filename'], directory + '/content/streams/' + row['filename'])
+
+
+def check_arguments():
+    # check for arguments
+    if len(sys.argv) < 3:
+        print("Please pass the input csv as the first argument and the output dir as the second argument")
+        print("Example:\n\tpython main.py \"example/1/inputs/xml/transfer.csv\" \"example/1/outputs\"")
+        sys.exit(1)
+    input_csv = sys.argv[1]
+    input_dir = os.path.dirname(input_csv)
+    output_dir = sys.argv[2]
+    output_dir = output_dir + "/" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    return input_csv, input_dir, output_dir
+
+
+def get_batches(rows):
+    batches = {}
+    for k, g in groupby(rows, lambda x: x['batch']):
+        batches[k] = []
+        for i in g:
+            batches[k].append(i)
+
+    return batches
+
+if __name__ == "__main__":
+    main()
