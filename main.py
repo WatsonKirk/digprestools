5dd44f9c053b08f1e7ba3ca8cde3c8b7db150969
Ryan Taylor <ryant@eng.utah.edu>
2017-08-03 08:27:09 -0600

    expect arguments for input file and output file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import csv
+import csv, sys, datetime
 from shutil import copyfile, move
 import os
 from template import fill_template, dc_template
@@ -6,24 +6,29 @@ from template import fill_template, dc_template
 # read input
 csv_file = 'transfer.csv'
 
-with open(csv_file, 'rb') as csvfile:
+input_csv = sys.argv[1]
+input_dir =  os.path.dirname(input_csv)
+output_dir = sys.argv[2]
+output_dir = output_dir + "/" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+with open(input_csv, 'rb') as csvfile:
     rows = csv.DictReader(csvfile)
     for row in rows:
         # first file
-        directory = row['filename'].replace('.tif', '')
+        directory = output_dir
         
         if not os.path.exists(directory):
             os.makedirs(directory + '/content/streams/')
 
-        move('../tifs/' +directory + '.tif', directory + '/content/streams')
+        copyfile( input_dir + '/../tifs/' + row['filename'], directory + '/content/streams/' + row['filename'])
         new_file = directory + '/content/ie.xml'
-        template = fill_template(row)
+        ie_template_string = fill_template(row)
 
         dc_file = directory + '/dc.xml'
         dc_template_string = dc_template(row)
 
         with open(new_file, 'w') as output_file:
-            output_file.write(template)
+            output_file.write(ie_template_string)
 
         with open(dc_file, 'w') as output_file:
             output_file.write(dc_template_string)
